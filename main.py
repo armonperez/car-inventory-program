6d5c429ae6b5e1b9ac79266e4f5c1cc0bcca2c02
armonperez <armonperez16@gmail.com>
2024-08-18 22:41:01 -0700

    Buy function now updates list to show a purchase

--- main.py
+++ main.py
@@ -2,22 +2,21 @@
 
 
 
-in_stock_Cars = {
+in_stock_Cars = [
 
-"toyota" : "corolla" ,
+"toyota corolla" ,
 
-"honda" : "civic" , 
-
-
-}
+"honda civic" , 
 
 
+]
 
-out_of_stock_cars = {
 
-"Lexus": "LS350"
+out_of_stock_cars = [ 
 
-}
+"Lexus LS350"
+
+]
 
 
 def question():
@@ -38,8 +37,18 @@ def question():
       return question
 
 def buy():
-    pass
+    print(in_stock_Cars)
+    purchase = input(f"Please select a car you would like to buy Option 1. {in_stock_Cars[0]} Option 2. {in_stock_Cars[1]}")
+    if purchase == "1":
+      pruchased_car = in_stock_Cars[0]
+      update_list =[item for item in out_of_stock_cars] + [pruchased_car]
+      in_stock_Cars.pop(0)
+
+      print("Out of stock cars" , update_list)
+      print("in stock cars", in_stock_Cars)
+      return buy
+
 def change_inventory():
     pass
 
-question()
+buy()
