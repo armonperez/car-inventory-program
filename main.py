43cdbe027a60cbe2e0d32b3b1e8c8442213f869c
armonperez <armonperez16@gmail.com>
2024-08-22 22:36:42 -0700

    added second car to the buy function.

--- main.py
+++ main.py
@@ -21,11 +21,13 @@ out_of_stock_cars = [
 
 def question():
 
-   answer = input("Do you want to purchase a car ?")
-   while True:
+  
+   while True: 
+    
+      answer = input("Do you want to purchase a car ?")
       if answer == "y":
        
-       print(f"We currently have the following in stock :{in_stock_Cars}")
+       print(f"We currently have the following in stock :{in_stock_Cars[0], in_stock_Cars[1]}")
 
        break
       if answer == "n":
@@ -34,21 +36,36 @@ def question():
 
       else:
        print("Invalid input")
+
       return question
 
 def buy():
     print(in_stock_Cars)
-    purchase = input(f"Please select a car you would like to buy Option 1. {in_stock_Cars[0]} Option 2. {in_stock_Cars[1]}")
+    purchase = input(f"Please select a car you would like to buy Option 1. {in_stock_Cars[0]} Option 2. {in_stock_Cars[1]} :" " ")
     if purchase == "1":
+      
+      print(f"You purchased a {in_stock_Cars[0]}.")
       pruchased_car = in_stock_Cars[0]
       update_list =[item for item in out_of_stock_cars] + [pruchased_car]
       in_stock_Cars.pop(0)
 
       print("Out of stock cars" , update_list)
       print("in stock cars", in_stock_Cars)
-      return buy
+      
+    elif purchase == "2":
+        print (f"You purchased a {in_stock_Cars[1]}")
+        pruchased_car = in_stock_Cars[1]
+        update_list =[item for item in out_of_stock_cars] + [pruchased_car]
+        in_stock_Cars.pop(1)
+        
+        return buy
 
 def change_inventory():
     pass
 
-buy()
+def main():
+   
+    question()
+    buy()
+
+main()
